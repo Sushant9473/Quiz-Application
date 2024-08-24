de526479a8027e38b92c00f264b6c23d5ed76cb4
Sushant <sushantsinghchauhan08@gmail.com>
2024-08-24 12:02:57 +0530

    small fixes

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "antd/dist/antd.min.css";
 import store from "./redux/store";
 import { Provider } from "react-redux";
@@ -13,5 +12,3 @@ root.render(
     <App />
   </Provider>
 );
-
-reportWebVitals();
